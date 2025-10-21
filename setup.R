005ac398696de531c8e6fbe477550cf577fd9980
Kendall OFarrell <kofarrell28@gmail.com>
2025-10-20 14:14:40 -1000

    coding exercises

---
+++ setup.R
@@ -0,0 +1,16 @@
+# setup script
+packageLoad <-
+  function(x) {
+    for (i in 1:length(x)) {
+      if (!x[i] %in% installed.packages()) {
+        install.packages(x[i])
+      }
+      library(x[i], character.only = TRUE)
+    }
+  }
+
+
+packageLoad(c("tidyverse", "palmerpenguins", "rmarkdown", "ggthemes",
+              "RColorBrewer", "viridis", "ggdark", "plotly", "lterdatasampler", 
+              "rstatix"))
+
